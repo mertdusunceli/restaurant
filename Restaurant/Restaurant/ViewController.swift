f5b5cd5672bf622e149063ee5a9af40fb6003898
mertdusunceli <mertdusunceli@gmail.com>
2018-05-30 22:27:10 -0700

    first
    me

---
+++ Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Mert Dusunceli on 5/30/18.
+//  Copyright © 2018 Mert Dusunceli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
